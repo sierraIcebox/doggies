28e50c792e1e8809d80bdda2ec876b6f0e98d118
Camille Sierra <sierra@iceboxmail.com>
2020-01-30 14:10:48 -0500

    dog display

--- src/components/Dogs/Dogs.js
+++ src/components/Dogs/Dogs.js
@@ -1,41 +1,67 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 // import DogSingle from './DogSingle';
 
 class Dogs extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            dogs:[],
-        };
-    }
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      dogs: [],
+    };
+  }
 
-    componentDidMount(){
-        const url = 'https://dog.ceo/api/breeds/list/all'
+  componentDidMount() {
+    const url = 'https://dog.ceo/api/breeds/list/all'
 
-        fetch(url)
-        .then((response) =>{
-            return response.json()
-        })
-        .then((data)=>{
-            
-                console.log(Object.keys(data.message))
-        })
-        .catch((error) => console.log(error))
-    }
-  
-        
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
 
-    render(){
-        return (
-            <ul>
-                {this.dogs}
-            </ul>
-            
-        )
-    }
+        const resultsArray = [];
+        const breedList = data.message
 
+
+        for (const breed in breedList) {
+          if (breedList[breed].length === 0) {
+            resultsArray.push(breed)
+          } else {
+
+            breedList[breed].forEach(subbreed => {
+              resultsArray.push(`${subbreed} ${breed}`)
+
+            });
+
+          }
+          // console.log(`${breed}: ${breedList[breed]}`)
+        }
+        this.setState(() => ({
+          dogs: resultsArray
+        }))
+      })
+
+      .catch((error) => console.log(error))
+  }
+
+
+
+  render() {
+    return ( 
+  <div>
+    {this.state.dogs.map((dog) => {
+      return (
+        <div>
+        {dog}
+      </div>
+      )
+   
+    })}
+  </div>
+    )
+  }
 }
 
 export default Dogs;
